3015cc79fd0f1566ff46b8e9084cbe2ba4fb00dc
Michael King <michael.ac.king@outlook.com>
2020-01-29 00:01:53 -0500

    Implement platform behaviour, reorganize assets, add a new level

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -7,8 +7,10 @@ public class Platform : MonoBehaviour
     [SerializeField] private Vector2 startPos = new Vector2(0, 0);
     [SerializeField] private Vector2 endPos = new Vector2(0, 0);
     [SerializeField] private float speed = 3f;
+    [SerializeField] private float stopDelay = 1f;
 
     private bool isGoingToEndPos = true;
+    private bool isStopped = false;
     private Color color;
     private ColorManager colorManager;
 
@@ -16,27 +18,37 @@ public class Platform : MonoBehaviour
     {
         colorManager = FindObjectOfType<ColorManager>();
         color = GetComponent<SpriteRenderer>().color;
+        StartCoroutine(Stop());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ((Vector2)transform.position == endPos)
+        if ((Vector2)transform.position == endPos && isGoingToEndPos)
         {
+            StartCoroutine(Stop());
             isGoingToEndPos = false;
         }
-        else if ((Vector2)transform.position == startPos)
+        else if ((Vector2)transform.position == startPos && !isGoingToEndPos)
         {
+            StartCoroutine(Stop());
             isGoingToEndPos = true;
         }
 
-        if (isGoingToEndPos)
+        if (isGoingToEndPos && !isStopped)
         {
             transform.position = Vector2.MoveTowards(transform.position, endPos, Time.fixedDeltaTime * speed);
         }
-        else 
+        else if (!isGoingToEndPos && !isStopped)
         {
             transform.position = Vector2.MoveTowards(transform.position, startPos, Time.fixedDeltaTime * speed);
         }
+    }
+
+    private IEnumerator Stop()
+    {
+        isStopped = true;
+        yield return new WaitForSeconds(stopDelay);
+        isStopped = false;
     }
 }
